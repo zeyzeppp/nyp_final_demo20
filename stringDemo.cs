430871a44c3edbc0c98ac18c219e1b66246e9fea
Zeynep Alperen <123326315+zeyzeppp@users.noreply.github.com>
2024-01-11 14:16:55 +0300

    Create stringDemo.cs

---
+++ stringDemo.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Data;
+using System.Xml;
+
+namespace final_calisma_demo
+{
+    class stringDemo
+    {
+        public static void Main()
+        {
+
+            string z;
+            Console.Write("Bir isim giriniz: ");
+            z = Console.ReadLine();
+
+            //Console.WriteLine("{0}", z.Length);
+
+            for(int i = z.Length - 1; i >= 0; i--)
+            {
+                Console.Write(z[i]);
+            }
+            
+
+
+        }
+
+        
+
+
+    }
+}
