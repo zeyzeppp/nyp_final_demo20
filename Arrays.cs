9a323dd6fc4b8e5f1ba29e8f735215c8a640e5b4
Zeynep Alperen <123326315+zeyzeppp@users.noreply.github.com>
2024-01-12 12:04:58 +0300

    Create Arrays.cs

---
+++ Arrays.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Data;
+using System.Xml;
+
+namespace final_calisma_demo
+{
+    class Arrays
+    {
+
+        public static void Main5()
+        {
+            int[] a;
+            int[] b;
+
+
+            a = new int[5] { 1, 2, 3, 4, 5 };
+
+            int n = 3;
+
+            // b = new int[n] { 0, 1, 2 }; --------------> ERROR !!!
+
+            int[] c = new int[n];
+
+
+
+        }
+
+
+    }
+
+}
